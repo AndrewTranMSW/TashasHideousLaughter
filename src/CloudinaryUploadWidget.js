ccb8b9d77bf00c03d41cee7d719df09d32171f27
Andrew Tran <andrewtranmsw@gmail.com>
2022-03-08 14:33:50 -0800

    uploading works, pushes to server

---
+++ src/CloudinaryUploadWidget.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class CloudinaryUploadWidget extends Component {
+  componentDidMount() {
+    var myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "ftchr",
+        uploadPreset: "ftchrnovowelsbaby",
+        sources: ["url", "camera", "local"],
+        cropping: true,
+        multiple: false,
+        defaultSource: "local",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+        }
+      }
+    );
+    document.getElementById("upload_widget").addEventListener(
+      "click",
+      function () {
+        myWidget.open();
+      },
+      false
+    );
+  }
+
+  render() {
+    return (
+      <button id="upload_widget" className="cloudinary-button">
+        Upload
+      </button>
+    );
+  }
+}
+
+export default CloudinaryUploadWidget;
